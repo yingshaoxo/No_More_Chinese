da7ac7dd13d64113946ba5ee872a1bdfdbb1ee64
yingshaoxo <yingshaoxo@gmail.com>
2018-03-24 12:07:57 +0800

    finished my work

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ let my_button = document.getElementById('my_button');
 
 my_button.onclick = function(element) {
     let color = element.target.value;
-    chrome.tabs.executeScript({code: 'alert("test");'});
+    chrome.tabs.executeScript({code: 'alert("It was made by yingshaoxo");'});
     /*chrome.tabs.executeScript({
         file: 'do.js'
     });*/
